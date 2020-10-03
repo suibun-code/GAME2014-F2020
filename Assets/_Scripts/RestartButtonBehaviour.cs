f4f1a3347e33a0c37d26e2b21fc40d6900ab5880
Ramin Amiri <zeuspsn.55555@gmail.com>
2020-10-03 00:03:54 -0400

    Finished Lesson 2
    Lesson 2 YouTube tutorial.

---
+++ Assets/_Scripts/RestartButtonBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButtonBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPressedButton()
+    {
+        Debug.Log("RestartButton Pressed");
+
+        SceneManager.LoadScene("Play");
+    }
+}
