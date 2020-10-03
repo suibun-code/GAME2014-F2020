463262c3400ea0a479e4e84f83e6719fac99761a
Ramin Amiri <zeuspsn.55555@gmail.com>
2020-10-03 01:27:28 -0400

    Lesson 4 Part 1

---
+++ Assets/_Scripts/TestButtonBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TestButtonBehaviour : MonoBehaviour
+{
+    public TMP_Text SceneLabel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnPressedButton()
+    {
+        Debug.Log("StartButton Pressed");
+        SceneLabel.text = "test";
+    }
+}
