75fb0281e0cc27d2e45df467f2d1261d423d941a
Ramin Amiri <zeuspsn.55555@gmail.com>
2020-10-27 08:42:55 -0400

    Collision with tilemap objects

---
+++ Assets/_Scripts/FrogScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogScript : MonoBehaviour
+{
+    public Vector3 frogPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Move(Vector3 newPos)
+    {
+        frogPos = newPos;
+        transform.position = frogPos;
+    }
+}
--- Assets/_Scripts/PlayerControls.cs
+++ Assets/_Scripts/PlayerControls.cs
@@ -25,22 +25,22 @@ public class PlayerControls : MonoBehaviour
         switch (direction)
         {
             case Direction.UP:
-                target.transform.position = new Vector3(target.transform.position.x, target.transform.position.y + 0.5f, target.transform.position.z);
+                target.GetComponent<FrogScript>().Move(new Vector3(target.transform.position.x, target.transform.position.y + 0.5f, target.transform.position.z));
                 Debug.Log("CLICKED UP");
                 break;
 
             case Direction.DOWN:
-                target.transform.position = new Vector3(target.transform.position.x, target.transform.position.y - 0.5f, target.transform.position.z);
+                target.GetComponent<FrogScript>().Move(new Vector3(target.transform.position.x, target.transform.position.y - 0.5f, target.transform.position.z));
                 Debug.Log("CLICKED DOWN");
                 break;
 
             case Direction.LEFT:
-                target.transform.position = new Vector3(target.transform.position.x - 0.5f, target.transform.position.y, target.transform.position.z);
+                target.GetComponent<FrogScript>().Move(new Vector3(target.transform.position.x - 0.5f, target.transform.position.y, target.transform.position.z));
                 Debug.Log("CLICKED LEFT");
                 break;
 
             case Direction.RIGHT:
-                target.transform.position = new Vector3(target.transform.position.x + 0.5f, target.transform.position.y, target.transform.position.z);
+                target.GetComponent<FrogScript>().Move(new Vector3(target.transform.position.x + 0.5f, target.transform.position.y, target.transform.position.z));
                 Debug.Log("CLICKED RIGHT");
                 break;
 
